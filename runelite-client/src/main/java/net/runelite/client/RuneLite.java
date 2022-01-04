4a6b3e55d77e54fccd84db0011d5836d4ce46f3e
9InchHog <whotfspoon@gmail.com>
2022-01-03 19:15:21 -0600

    oprs -> runelite

--- runelite-client/src/main/java/net/runelite/client/RuneLite.java
+++ runelite-client/src/main/java/net/runelite/client/RuneLite.java
@@ -327,7 +327,7 @@ public class RuneLite
 		{
 			log.error("Failure during startup", e);
 			SwingUtilities.invokeLater(() ->
-				new FatalErrorDialog("OpenOSRS has encountered an unexpected error during startup.")
+				new FatalErrorDialog("RuneLite has encountered an unexpected error during startup.")
 					.open());
 		}
 		finally
